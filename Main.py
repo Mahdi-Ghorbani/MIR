805ff659b2d31ed049b13e26618e6f213e46e22b
Mahdi Ghorbani <m4hd1gh0@gmail.com>
2019-11-29 01:31:05 +0330

    Variable byte compression implemented

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from preprocessor import EnglishProcessor, PersianProcessor
 from indexer import Positional, Bigram
-from compressor import GamaCode, VariableByte
+from compressor import GammaCode, VariableByte
 from query_editor import Editor
 from searcher import TF_IDF
 
@@ -12,8 +12,11 @@ if __name__ == '__main__':
     # TODO: Should we take care about the Titles (Not only Text)?
     english_preprocessor.preprocess(english_df['Text'])
 
-    positional_index = Positional(english_preprocessor=english_preprocessor)
+    positional_index = Positional(preprocessor=english_preprocessor)
     positional_index.add_df(english_df['Text'])
+
+    vb_compressor = VariableByte(positional_index=positional_index.index)
+    vb_compressor.compress()
 
     while True:
         print("Please enter the command number:")
@@ -26,6 +29,10 @@ if __name__ == '__main__':
         print("6. Remove a document by document ID from positional index")
         print("7. Save the positional index in a file")
         print("8. Load a positional index from a file")
+        print("9. Print the variable byte encoded format")
+        print("10. Save the variable byte encoded format in a file")
+        print("11. Load a variable byte encoded format from a file")
+        print("12. Compare between used space before and after variable byte compression")
         cmd = int(input())
         if cmd == 0:
             break
@@ -57,3 +64,15 @@ if __name__ == '__main__':
             print("Enter the name of the file")
             cmd = input()
             positional_index.load_from_file(cmd)
+        elif cmd == 9:
+            vb_compressor.print_result()
+        elif cmd == 10:
+            print("Enter the name of the file")
+            cmd = input()
+            vb_compressor.save_to_file(cmd)
+        elif cmd == 11:
+            print("Enter the name of the file")
+            cmd = input()
+            vb_compressor.load_from_file(cmd)
+        elif cmd == 12:
+            vb_compressor.print_used_space()
--- compressor.py
+++ compressor.py
@@ -1,19 +1,79 @@
+import pickle
+from sys import getsizeof
+
+
 class VariableByte:
 
-    def compress(self, index):
+    def __init__(self, positional_index):
+        self.index = positional_index
+        self.compressed = {}
+
+    def vb_encode(self, number):
+        vb = []
+        while True:
+            binary = bin(number % 128)[2:]
+            number = number // 128
+            if number == 0:
+                binary = '1' + '0' * (8 - len(binary) - 1) + binary
+                vb.append(int(binary, 2))
+                break
+            else:
+                binary = '0' * (8 - len(binary)) + binary
+                vb.append(int(binary, 2))
+        return bytes(vb)
+
+    def vb_decode_index(self):
         pass
 
-    def save_to_file(self):
-        pass
+    def compress(self):
+        # compressed is a mapping from term to a dictionary which is a mapping from df to its value and
+        # a mapping from posting to an array which its first element is vb encoding of the doc_ids and
+        # the second element is an array where each element in that is the vb encoding of the positions
+        # {'hi': {'df': 2, 'posting': {1: [2, 5], 3: [7, 13]}}}  ---->
+        # {'hi': {'df': 2, 'posting': [\x81 \x82, [\x82 \x83, \x87 \x86]]}}
+        compressed = {}
+        for term in self.index:
+            compressed[term] = {'df': self.index[term]['df'], 'posting': [bytes(0), []]}
+            prev_doc = -1
+            for doc_id in self.index[term]['posting']:
+                if prev_doc == -1:
+                    enc = self.vb_encode(doc_id)
+                else:
+                    enc = self.vb_encode(doc_id - prev_doc)
+                compressed[term]['posting'][0] += enc
+                prev_doc = doc_id
+                prev_pos = -1
+                for pos in self.index[term]['posting'][doc_id]:
+                    if prev_pos == -1:
+                        compressed[term]['posting'][1].append(self.vb_encode(pos))
+                    else:
+                        compressed[term]['posting'][1][len(compressed[term]['posting'][1]) - 1] += \
+                            self.vb_encode(pos - prev_pos)
+                    prev_pos = pos
+        self.compressed = compressed
+        return compressed
 
-    def load_from_file(self):
-        pass
+    def save_to_file(self, name):
+        with open(name, 'wb') as f:
+            pickle.dump(self.compressed, f)
+            f.close()
+
+    def load_from_file(self, name):
+        with open(name, 'rb') as f:
+            self.compressed = pickle.load(f)
+            f.close()
 
     def print_result(self):
-        pass
+        print(self.compressed)
+
+    def print_used_space(self):
+        print("Size of index before compressing:", getsizeof(pickle.dumps(self.index)), "bytes")
+        print("Size of index after variable byte compressing:", getsizeof(pickle.dumps(self.compressed)), "bytes")
+        print("Amount of reduction:", getsizeof(pickle.dumps(self.index)) - getsizeof(pickle.dumps(self.compressed)),
+              "bytes")
 
 
-class GamaCode:
+class GammaCode:
 
     def compress(self, index):
         pass
